d35099c3fd0ecec74c0ca5af2a60b2791aa2945e
smmuke123 <edgarariza6@outlook.com>
2025-07-14 00:38:27 -0400

    Se agregan las clases hasta el video 14 del curso

---
+++ OperadoresDeComparación/document.js
@@ -0,0 +1,15 @@
+let valor1, valor2;
+valor1 = 10;
+valor2 = 20;
+
+resultado = valor1 > valor2;
+
+document.getElementById("resultado").innerHTML = "<em> El resultado es: " + resultado + "</em> <br>";
+
+let resultado2 = valor1 < valor2;
+
+document.getElementById("resultado2").innerHTML += "<em> El resultado es: " + resultado2 + "</em> <br>";
+
+let resultado3 = valor1 == valor2;
+
+document.getElementById("resultado3").innerHTML += "<em> El resultado es: " + resultado3 + "</em> <br>";
